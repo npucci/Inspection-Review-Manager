4937ca5b5d1832c2eb9ce91aececaab6e0856935
Nicco Pucci <npucci@sfu.ca>
2017-08-03 13:11:26 -0700

    Updated layouts used in MainActivity and InspectionReviewActivity

--- app/src/main/java/com/example/nicco/inspectionReviewManager/MainActivity.java
+++ app/src/main/java/com/example/nicco/inspectionReviewManager/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.nicco.inspectionReviewManager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ItemDecoration;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,9 +23,15 @@ public class MainActivity extends FragmentActivity {
 
         RecyclerView archive = (RecyclerView) findViewById(R.id.recyclerViewArchive);
         archive.setAdapter(new RecyclerAdapter(this, model.getDatabaseCursor()));
-        archive.setHasFixedSize(true);
+        archive.setHasFixedSize(false);
         archive.setLayoutManager(new LinearLayoutManager(this));
         archive.setItemAnimator(new DefaultItemAnimator());
+        archive.setBackgroundColor(Color.DKGRAY);
+//        DividerItemDecoration  dividerItemDecoration = new DividerItemDecoration (
+//                archive.getContext(), getResources().getConfiguration().orientation
+//
+//        );
+//        archive.addItemDecoration(dividerItemDecoration);
 
         final Button editReviewReviewButton = (Button) findViewById(R.id.buttonEditReview);
         editReviewReviewButton.setOnClickListener(new View.OnClickListener() {
@@ -46,4 +55,15 @@ public class MainActivity extends FragmentActivity {
             }
         });
     }
+
+    private class DividerItemDecoration extends ItemDecoration {
+        private Context context;
+        private int orientation;
+
+        public DividerItemDecoration(Context context, int orientation) {
+            this.context = context;
+            this.orientation = orientation;
+        }
+    }
+
 }
